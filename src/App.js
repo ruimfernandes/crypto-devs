0ad6a50b0cb52f7f945d5aae10527778f4a3191c
Rui Fernandes <rui.fernandes@utrust.com>
2021-11-15 20:22:45 +0000

    Create company

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { getAddress, getCompanyContract, setupWeb3 } from "./web3Client";
+import {
+  createCompany,
+  getAddress,
+  getCompany,
+  getCompaniesCount,
+  setupWeb3,
+} from "./web3Client";
 import Company from "./components/Company";
 
 import "./App.css";
@@ -7,21 +13,53 @@ import "./App.css";
 function App() {
   const [address, setAddress] = useState("N/A");
   const [company, setCompany] = useState(null);
+  const [name, setName] = useState("");
 
   useEffect(() => {
-    const web3 = setupWeb3();
-    getAddress(web3).then((addresses) => setAddress(addresses[0]));
-    getCompanyContract(web3).then(contract => {
-      console.log(contract)
-      contract.methods.companies(1).call().then(r => setCompany(r))
-    });
+    const fetchInitialData = async () => {
+      await setupWeb3();
+      const address = await getAddress();
+
+      setAddress(address);
+      fetchCompanies();
+    };
+
+    fetchInitialData();
   }, []);
+
+
+  const fetchCompanies = async () => {
+    const companies_count = await getCompaniesCount();
+    const company = await getCompany(1);
+    console.log(companies_count);
+    setCompany(company);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("The name you entered was: ", name);
+    createCompany(address, name);
+  };
 
   return (
     <div className="App">
       <h1>Crypto devs</h1>
       <span>{address}</span>
       <Company company={company}></Company>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h3>Insert company</h3>
+          <label>
+            Name:{" "}
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </label>
+          <input type="submit" value="Create" />
+        </form>
+      </div>
     </div>
   );
 }
--- src/web3Client.js
+++ src/web3Client.js
@@ -1,17 +1,34 @@
 import Web3 from "web3";
 import CompaniesContractBuild from 'contracts/build/contracts/Companies.json';
 
-async function getAddress(web3) {
-  return web3.eth.getAccounts();
+let web3;
+let companyContract;
+
+async function setupWeb3() {
+  web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+  companyContract = await getCompanyContract();
+  return web3;
 }
 
-async function getCompanyContract(web3) {
+async function createCompany(address, name) {
+  return companyContract.methods.createCompany(name).send({from: address}).then(r => r)
+};
+
+async function getCompaniesCount() {
+  return companyContract.methods.company_count().call().then(r => r)
+};
+
+async function getCompany(id) {
+  return companyContract.methods.companies(id).call().then(r => r)
+};
+
+async function getAddress() {
+  return web3.eth.getAccounts().then((addresses) => addresses[0]);
+}
+
+async function getCompanyContract() {
   const networkId = await web3.eth.net.getId();
   return new web3.eth.Contract(CompaniesContractBuild.abi, CompaniesContractBuild.networks[networkId].address);
 }
 
-function setupWeb3() {
-  return new Web3(Web3.givenProvider || "http://localhost:7545");
-}
-
-export {getAddress, getCompanyContract, setupWeb3};
+export {createCompany, getAddress, getCompany, getCompaniesCount, setupWeb3};
